d64e9069a255b7ffe0e91322c910f4548ae5d8bd
aw18 <aw18@sanger.ac.uk>
2013-07-04 16:38:28 +0100

    changed the replace templatetag from using the replace() to using a re.sub() to eliminate all symbols that are non-words to underscore

--- DNAOrderApp/order/templatetags/replace.py
+++ DNAOrderApp/order/templatetags/replace.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+import re
 
 # To be a valid tag library, module must contain a module-level variable named register
 # in which all the tags and filters are registered.
@@ -8,10 +9,8 @@ register = template.Library()
 # Taking in a variable and replacing it with the argument
 @stringfilter #only expects a string as first argument; converts an object to its string value 
 def replace(value):
-	"""Taking in a string value and replacing whitespace with underscores"""
-	print "in replace!!!!!!"
-	print value.replace(" ", "_")
-	return value.replace(" ", "_")
+	"""Taking in a string value and replacing any non-words with underscores, i.e. spaces, ;:/?:@=&"""
+	return re.sub(r'[^\w]', '_', value)
 
 # register filter definition with the Library instance, to make it available to Django's template language
 register.filter('replace', replace) 
