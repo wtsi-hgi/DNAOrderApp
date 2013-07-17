5e9f2d56a9c7c25d162570e06864c1a68ddad4d8
aw18 <aw18@sanger.ac.uk>
2013-07-17 23:36:46 +0100

    added two templatetags, creating ai forms and creating ai formset, the create_formset_list.py is not used. Decided not to use formset even though it is used for setting up multiple instances of the same form. I can't seem to be able to 'pop' a form out in the right location. There are simply too many loops. Decided to create individual forms instead.

---
+++ DNAOrderApp/order/templatetags/create_aiform.py
@@ -0,0 +1,16 @@
+from django import template
+from django.forms import ModelForm
+from DNAOrderApp.order.models import AffiliatedInstituteForm
+
+register = template.Library()
+
+def create_aiform(projid):
+	"""You call this whenever you want an aiform to be made"""
+	aiform = AffiliatedInstituteForm(projid=projid)
+	aiform = aiform.as_p()
+	print "this is aiform", aiform
+	return aiform
+
+register.filter('create_aiform', create_aiform)
+
+# prefix=str(projid)
