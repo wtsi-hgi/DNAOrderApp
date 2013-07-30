12585f78ef42bbd044a169ec69251051baf1f17b
aw18 <aw18@sanger.ac.uk>
2013-07-30 14:05:38 +0100

    before I start rewriting the form submissions

--- DNAOrderApp/order/urls.py
+++ DNAOrderApp/order/urls.py
@@ -26,7 +26,8 @@ urlpatterns = patterns('',
     # url(r'^admin-page/(?P<table>[^/]+)/(?P<pkid>\d+)/$', views.admin_page, name='admin-page-table-pkid'),
 
     # Temp SS page
-    url(r'^add_tmp_ss/(?P<projid>\d+)?/?', views.add_tmp_sample_submission, name='add_tmp_ss'), #sample name and sample num
+    url(r'^handle_tss_pages/(?P<action>\w+)/?(?P<projid>\d+)?/?(?P<tssid>\d+)?/?$', views.handle_tss_pages, name="handle_tss_pages"), #next button
+    url(r'^tss-page-1/(?P<projid>\d+)?/?', views.tss_page_1, name='tss-page-1'), #sample name and sample num
     url(r'^tss-page-2/?(?P<tssid>\d+)?/?$', views.tss_page_2, name='tss-page-2'), #phenotype list
     url(r'^tss-page-3/?(?P<tssid>\d+)?/?$', views.tss_page_3, name='tss-page-3'), #affiliated institute 
     url(r'^tss-page-4$', views.tss_page_4, name='tss-page-4'), #personnels
