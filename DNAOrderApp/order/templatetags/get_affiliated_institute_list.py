6f5053fec4fc83b4957ab9dbd0c5004c75af3e3c
aw18 <aw18@sanger.ac.uk>
2013-08-01 16:54:12 +0100

    added the fil ter, get_all_affiliated_institute_for_tss4, so to convert the list of affiliated institute into an html unordered list

--- DNAOrderApp/order/templatetags/get_affiliated_institute_list.py
+++ DNAOrderApp/order/templatetags/get_affiliated_institute_list.py
@@ -3,8 +3,18 @@ from DNAOrderApp.order.models import AffiliatedInstitute
 
 register = template.Library()
 
+def get_all_affiliated_institute_for_tss4(QuerySet_Affiliated_Institute):
+	"""Returns all affiliated institute for tmp-sample-submission-4.html"""
+	ai_list = QuerySet_Affiliated_Institute.all()
+	html_string = "<ul>"
+	for ai in ai_list:
+		html_string += "<li>" + ai.ai_name + "</li>"
+	html_string+="</ul>"
+	print "inside get_all_affiliated_institute with queryset affiliated institute"
+	return html_string
+
 def get_all_affiliated_institute(Queryset_Affiliated_Institute):
-	"""Returns all affiliated institute"""
+	"""Returns all affiliated institute for typeahead function in fmprojectlist"""
 	ai_list = []
 	for c in Queryset_Affiliated_Institute:
 		print str(c.ai_name)
@@ -17,5 +27,6 @@ def get_affiliated_institute_by_contact(dnaorderappuser):
 	"""Returns the affiliated institute associated to the DNAOrderAppUser"""
 	return AffiliatedInstitute.objects.filter(dnaorderappuser__username__exact=dnaorderappuser)
 
+register.filter('get_all_affiliated_institute_for_tss4', get_all_affiliated_institute_for_tss4)
 register.filter('get_all_affiliated_institute', get_all_affiliated_institute)
 register.filter('get_affiliated_institute_by_contact', get_affiliated_institute_by_contact)
